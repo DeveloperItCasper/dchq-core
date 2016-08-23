faad901012b5920fec773db9c3f7295ac626c8b2
fireworrks <fireworrkz@live.com.au>
2016-08-23 21:36:05 +1000

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -12,6 +12,8 @@ class Address < ActiveRecord::Base
       v.validates :state
       v.validates :post_code
     end
+      o.validates :country_code, inclusion: { in: CountrySelect::COUNTRIES.keys }
+  end
 
   attr_accessible :first, :second, :city, :state, :country_code, :post_code
 
